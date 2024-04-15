ef5b691036aaa7edfd91025c0980db58de154e06
Ilya Raykker <ilya.th.kapitonov@gmail.com>
2024-04-15 11:33:55 +0400

    Setup test environment

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+import type { JestConfigWithTsJest } from 'ts-jest';
+
+const jestConfig: JestConfigWithTsJest = {
+    preset: 'ts-jest',
+    verbose: true,
+    testTimeout: 10000,
+    coveragePathIgnorePatterns: ['./src/dal/graphclients'],
+};
+
+export default jestConfig;
