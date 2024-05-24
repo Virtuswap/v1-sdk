6d128cbf8d2802a6f101f07badf35364d7fe7d09
Ilya Raykker <ilya.th.kapitonov@gmail.com>
2024-05-24 16:58:55 +0400

    Improve dal tests: increase coverage and add mocks

--- tests/dal.test.ts
+++ tests/dal.test.ts
@@ -1,57 +1,266 @@
 import { getBlockNumber, getBlockTimestamp } from '../src/dal/meta';
-import { getAllPairs } from '../src/dal/pairs';
-import { queryAllPairs, queryMeta } from '../src/dal/utils/execute';
+import { getAllPairs, getTopPairsForToken } from '../src/dal/pairs';
+import { getAllTokens } from '../src/dal/tokens';
+import {
+    queryAllPairs,
+    queryMeta,
+    queryAllTokens,
+} from '../src/dal/utils/execute';
 import { Chain } from '../src/entities/chain';
+import {
+    mockAllPairsResponse,
+    mockAllTokensResponse,
+    mockMetadataResponse,
+    mockTokensPricesResponse,
+} from './mocks';
+import { Address } from '../src/entities/utils';
+
+jest.mock('../src/utils/pricing', () => {
+    return {
+        getMultipleTokensPriceUsd: jest.fn(
+            (_: Chain, tokensAddresses: Array<string>) => {
+                return mockTokensPricesResponse
+                    .filter((t) =>
+                        tokensAddresses.find(
+                            (addr) => addr.toLowerCase() == t.id.toLowerCase()
+                        )
+                    )
+                    .map((t) => parseFloat(t.priceUsd));
+            }
+        ),
+    };
+});
+
+jest.mock('axios', () => {
+    return {
+        get: jest.fn((urlString: string) => {
+            const url = new URL(urlString);
+            const params = new URLSearchParams(url.search);
+            let errResponse = {
+                status: 400,
+                statusText: 'Bad Request',
+                data: {},
+            };
+            if (!params.has('chainId')) {
+                errResponse.data = {
+                    error: "missing required parameter 'chainId'",
+                };
+                return errResponse;
+            }
+            const chainId = parseInt(params.get('chainId')!);
+            // hardcoded statuses for testing purposes
+            if (chainId == 987654321) {
+                throw new Error('axios mock error');
+            } else if (chainId == 123456789) {
+                errResponse.data = { error: 'test code 400' };
+                return errResponse;
+            }
+
+            let response = {
+                status: 200,
+                statusText: 'OK',
+                data: {},
+            };
+            const urlWithoutParams = url.origin + url.pathname;
+            switch (urlWithoutParams.toLowerCase()) {
+                case 'https://api.virtuswap.io/graph/pairs':
+                    response.data = mockAllPairsResponse;
+                    break;
+                case 'https://api.virtuswap.io/graph/meta':
+                    response.data = mockMetadataResponse;
+                    break;
+                case 'https://api.virtuswap.io/graph/tokens':
+                    response.data = mockAllTokensResponse;
+                    break;
+                default:
+                    errResponse.status = 404;
+                    errResponse.statusText = 'Not Found';
+                    errResponse.data = {
+                        message: 'Route not found',
+                        error: 'Not Found',
+                        statusCode: 404,
+                    };
+                    return errResponse;
+            }
+            return response;
+        }),
+    };
+});
 
 describe('DAL: queries', () => {
-    test('queryAllPairs works with polygon-testnet', async () => {
-        const pairs = await queryAllPairs(Chain.POLYGON_TESTNET);
-        expect(pairs.length).toBeGreaterThan(0);
+    test('queryAllPairs works', async () => {
+        for (const chain of Object.values(Chain)) {
+            let pairs = await queryAllPairs(chain as Chain);
+            expect(pairs.length).toEqual(mockAllPairsResponse.length);
+        }
     });
-    test('queryMeta works with polygon-testnet', async () => {
-        const meta = await queryMeta(Chain.POLYGON_TESTNET);
-        expect(meta?.block.timestamp).not.toBeNull;
-        expect(meta?.block.number).not.toBeNull;
+    test('queryAllPairs returns empty array if unsupported chain id is provided', async () => {
+        let pairs = await queryAllPairs(123456789 as Chain);
+        expect(pairs.length).toEqual(0);
+        pairs = await queryAllPairs(987654321 as Chain);
+        expect(pairs.length).toEqual(0);
     });
-    test('queryAllPairs works with arbitrum-testnet', async () => {
-        const pairs = await queryAllPairs(Chain.ARBITRUM_TESTNET);
-        expect(pairs.length).toBeGreaterThan(0);
+    test('queryMeta works', async () => {
+        for (const chain of Object.values(Chain)) {
+            let meta = await queryMeta(chain as Chain);
+            expect(meta!.block.timestamp).toEqual(
+                mockMetadataResponse.block.timestamp
+            );
+            expect(meta!.block.number).toEqual(
+                mockMetadataResponse.block.number
+            );
+        }
     });
-    test('queryMeta works with arbitrum-testnet', async () => {
-        const meta = await queryMeta(Chain.ARBITRUM_TESTNET);
-        expect(meta?.block.timestamp).not.toBeNull;
-        expect(meta?.block.number).not.toBeNull;
+    test('queryMeta returns null if unsupported chain id is provided', async () => {
+        let meta = await queryMeta(123456789 as Chain);
+        expect(meta).toBeNull;
+        meta = await queryMeta(987654321 as Chain);
+        expect(meta).toBeNull;
+    });
+    test('queryAllTokens works', async () => {
+        for (const chain of Object.values(Chain)) {
+            let tokens = await queryAllTokens(chain as Chain);
+            expect(tokens.length).toEqual(mockAllTokensResponse.length);
+        }
+    });
+    test('queryAllTokens returns empty array if unsupported chain id is provided', async () => {
+        let tokens = await queryAllTokens(123456789 as Chain);
+        expect(tokens.length).toEqual(0);
+        tokens = await queryAllTokens(987654321 as Chain);
+        expect(tokens.length).toEqual(0);
     });
 });
 
 describe('DAL: Metadata', () => {
-    /* TODO: redeploy polygon on Amoy testnet
-    test('getBlockNumber works with polygon-testnet', async () => {
-        const blockNumber = await getBlockNumber(Chain.POLYGON_TESTNET);
-        expect(blockNumber).toBeGreaterThan(48141038);
+    test('getBlockNumber works', async () => {
+        for (const chain of Object.values(Chain)) {
+            let blockNumber = await getBlockNumber(chain as Chain);
+            expect(blockNumber).toEqual(40);
+        }
+        let blockNumber = await getBlockNumber(123456789 as Chain);
+        expect(blockNumber).toEqual(0);
     });
-    test('getBlockTimestamp works with polygon-testnet', async () => {
-        const blockTimestamp = await getBlockTimestamp(Chain.POLYGON_TESTNET);
-        expect(blockTimestamp).toBeGreaterThan(1712929630);
-    });
-    */
-    test('getBlockNumber works with arbitrum-testnet', async () => {
-        const blockNumber = await getBlockNumber(Chain.ARBITRUM_TESTNET);
-        expect(blockNumber).toBeGreaterThan(32943564);
-    });
-    test('getBlockTimestamp works with arbitrum-testnet', async () => {
-        const blockTimestamp = await getBlockTimestamp(Chain.ARBITRUM_TESTNET);
-        expect(blockTimestamp).toBeGreaterThan(1712929569);
+    test('getBlockTimestamp works', async () => {
+        for (const chain of Object.values(Chain)) {
+            let blockTimestamp = await getBlockTimestamp(chain as Chain);
+            expect(blockTimestamp).toEqual(400);
+        }
+        let blockTimestamp = await getBlockTimestamp(123456789 as Chain);
+        expect(blockTimestamp).toEqual(0);
     });
 });
 
 describe('DAL: Pairs', () => {
-    test('getAllPairs works with polygon-testnet', async () => {
-        const pairs = await getAllPairs(Chain.POLYGON_TESTNET);
-        expect(pairs.length).toBeGreaterThan(0);
+    test('getAllPairs works', async () => {
+        for (const chain of Object.values(Chain)) {
+            const pairs = await getAllPairs(chain as Chain);
+            for (const [i, mockPair] of mockAllPairsResponse.entries()) {
+                expect(mockPair.id.toLowerCase()).toEqual(
+                    pairs[i].address.toString()
+                );
+                expect(mockPair.balance0).toEqual(pairs[i].token0.balance);
+                expect(mockPair.balance1).toEqual(pairs[i].token1.balance);
+                expect(mockPair.blocksDelay).toEqual(
+                    pairs[i].blocksDelay.toString()
+                );
+                expect(mockPair.lastSwapBlock).toEqual(
+                    pairs[i].lastSwapBlock.toString()
+                );
+                expect(mockPair.lastSwapTimestamp).toEqual(
+                    pairs[i].lastSwapTimestamp.toString()
+                );
+                expect(mockPair.fee).toEqual(pairs[i].fee.toString());
+                expect(mockPair.vFee).toEqual(pairs[i].vFee.toString());
+                expect(mockPair.maxReserveRatio).toEqual(
+                    pairs[i].maxReserveRatio.toString()
+                );
+                expect(mockPair.reserveRatio).toEqual(
+                    pairs[i].reserveRatio.toString()
+                );
+                for (const [j, mockWhitelistEntry] of mockAllPairsResponse[
+                    i
+                ].whitelist.entries()) {
+                    expect(mockWhitelistEntry.token.id.toLowerCase()).toEqual(
+                        pairs[i].allowList[j].toString()
+                    );
+                }
+                for (const [j, mockReserveEntry] of mockAllPairsResponse[
+                    i
+                ].pairReserves.entries()) {
+                    expect(mockReserveEntry.token.id.toLowerCase()).toEqual(
+                        pairs[i].reserves[j].reserveToken.address.toString()
+                    );
+                    expect(mockReserveEntry.token.decimals).toEqual(
+                        pairs[i].reserves[j].reserveToken.decimals.toString()
+                    );
+                    expect(mockReserveEntry.balance).toEqual(
+                        pairs[i].reserves[j].reserveToken.balance
+                    );
+                    expect(mockReserveEntry.baseValue).toEqual(
+                        pairs[i].reserves[j].baseToken.balance
+                    );
+                }
+            }
+        }
+        const pairs = await getAllPairs(123456789 as Chain);
+        expect(pairs.length).toEqual(0);
     });
-    test('getAllPairs works with arbitrum-testnet', async () => {
-        const pairs = await getAllPairs(Chain.ARBITRUM_TESTNET);
-        expect(pairs.length).toBeGreaterThan(0);
+    test('getTopPairsForToken works', async () => {
+        const expectedPairs = mockAllPairsResponse.filter(
+            (p) =>
+                p.id == '0xd0b12762620af08ddcf9c72f61d0d54c865b46ef' ||
+                p.id == '0x4917459762e7fbb93cea11960198ffb3dd3572b6'
+        )!;
+        expect(expectedPairs.length).toEqual(2);
+        for (const chain of Object.values(Chain)) {
+            let pairs = await getTopPairsForToken(
+                chain as Chain,
+                new Address('0x4303d1d547fafa114f90b85356275c89465a60bf'),
+                0
+            );
+            expect(pairs).toEqual([]);
+            pairs = await getTopPairsForToken(
+                chain as Chain,
+                new Address('0x4303d1d547fafa114f90b85356275c89465a60bf'),
+                1
+            );
+            expect(pairs[0].address.toString()).toEqual(expectedPairs[1].id);
+            pairs = await getTopPairsForToken(
+                chain as Chain,
+                new Address('0x4303d1d547fafa114f90b85356275c89465a60bf'),
+                2
+            );
+            expect(pairs[0].address.toString()).toEqual(expectedPairs[1].id);
+            expect(pairs[1].address.toString()).toEqual(expectedPairs[0].id);
+            pairs = await getTopPairsForToken(
+                chain as Chain,
+                new Address('0x4303d1d547fafa114f90b85356275c89465a60bf'),
+                10
+            );
+            expect(pairs[0].address.toString()).toEqual(expectedPairs[1].id);
+            expect(pairs[1].address.toString()).toEqual(expectedPairs[0].id);
+        }
+        const pairs = await getTopPairsForToken(
+            123456789 as Chain,
+            new Address('0x4303d1d547fafa114f90b85356275c89465a60bf'),
+            10
+        );
+        expect(pairs.length).toEqual(0);
+    });
+});
+
+describe('DAL: Tokens', () => {
+    test('getAllTokens works', async () => {
+        for (const chain of Object.values(Chain)) {
+            const tokens = await getAllTokens(chain as Chain);
+            for (const [i, mockToken] of mockAllTokensResponse.entries()) {
+                expect(mockToken.id.toLowerCase()).toEqual(
+                    tokens[i].address.toString()
+                );
+                expect(mockToken.decimals).toEqual(tokens[i].decimals);
+            }
+        }
+        const tokens = await getAllTokens(123456789 as Chain);
+        expect(tokens.length).toEqual(0);
     });
 });
--- tests/mocks.ts
+++ tests/mocks.ts
@@ -477,7 +477,83 @@ export const mockAllPairsResponse = [
 
 export const mockMetadataResponse = {
     block: {
-        number: '40',
-        timestamp: '400',
+        number: 40,
+        timestamp: 400,
     },
 };
+
+export const mockAllTokensResponse = [
+    {
+        id: '0x0a9bc7c7721297da60e9510a1682fca347bf70e5',
+        symbol: 'WBTC',
+        name: 'WBTC Test Token',
+        decimals: '8',
+    },
+    {
+        id: '0x220db2b860f817d43b6ee2733735274e51e7a491',
+        symbol: 'USDC',
+        name: 'USDC Test Token',
+        decimals: '6',
+    },
+    {
+        id: '0x4303d1d547fafa114f90b85356275c89465a60bf',
+        symbol: 'USDT',
+        name: 'USDT Test Token',
+        decimals: '6',
+    },
+    {
+        id: '0x58e948c6e48d3ebc909332e22972101425109bf4',
+        symbol: 'VRSW',
+        name: 'VirtuSwap Test Token',
+        decimals: '18',
+    },
+    {
+        id: '0x6c06aa45c300a55a810b4b36c73d861cc8708833',
+        symbol: 'DAI',
+        name: 'DAI Test Token',
+        decimals: '18',
+    },
+    {
+        id: '0x94b47ebe537c9ac7b37bdf7a5f68e7b6a1f16eab',
+        symbol: 'WETH',
+        name: 'WETH Test Token',
+        decimals: '18',
+    },
+    {
+        id: '0xe2e5d19e1d498130583ce21640c59a24cce83e17',
+        symbol: 'WMATIC',
+        name: 'WMATIC Test Token',
+        decimals: '18',
+    },
+];
+
+export const mockTokensPricesResponse = [
+    {
+        id: '0x0a9bc7c7721297da60e9510a1682fca347bf70e5',
+        priceUsd: '67000',
+    },
+    {
+        id: '0x220db2b860f817d43b6ee2733735274e51e7a491',
+        priceUsd: '1.01',
+    },
+    {
+        id: '0x4303d1d547fafa114f90b85356275c89465a60bf',
+        priceUsd: '0.99',
+    },
+    {
+        id: '0x58e948c6e48d3ebc909332e22972101425109bf4',
+        priceUsd: '0.002',
+    },
+    {
+        id: '0x6c06aa45c300a55a810b4b36c73d861cc8708833',
+        priceUsd: '1',
+    },
+    {
+        id: '0x94b47ebe537c9ac7b37bdf7a5f68e7b6a1f16eab',
+        priceUsd: '3123',
+    },
+    {
+        id: '0xe2e5d19e1d498130583ce21640c59a24cce83e17',
+        priceUsd: '2.34',
+    },
+];
